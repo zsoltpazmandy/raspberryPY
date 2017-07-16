10e75e03949cc32be388a1833c420720a351b464
Zsolt Pazmandy <zsoltpazmandy@gmail.com>
2017-07-16 18:23:28 +0100

    initial commit, added basic wrapper functionality: rotate+mirror pattern for 1:1 data transfer for the GPIO

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # raspberryPY
+#### passtime project
+##### Control wrapper for the GPIO Pi-Lite dot-matrix display for the Raspberry PI
+The GPIO display is a 14 * 9 LED-matrix one. Data is sent to the display via a 126-bit sequence iterating form the top left: vertically down on the Y axis then column by column on the X e.g.:
+```
+  the data (line-broken for visibility)     is displayed as follows:
+   
+                000000001                       00000000x00000
+                000000010                       0000000x000000  
+                000000100                       000000x0000000  
+                000001000                       00000x00000000  
+                000010000                       0000x000000000  
+                000100000                       000x0000000000  
+                001000000                       00x00000000000  
+                010000000                       0x000000000000  
+                100000000                       x0000000000000  
+                000000000
+                000000000
+                000000000
+                000000000
+                000000000
+```
+For more easy-to-use data input, therefore, a simple matrix rotation function is used: rotate once clock-wise, then mirror around the Y axis.
---
+++ gpio_wrapper.py
@@ -0,0 +1,82 @@
+import time
+import sys
+import serial
+import numpy
+
+serial = serial.Serial()
+serial.baudrate = 9600
+serial.timeout = 0
+serial.port = "/dev/serial0"
+
+
+# function to initialise serial port and communication with the GPIO display
+def init():
+    try:
+        serial.open()
+    except serial.SerialException, exception:
+        sys.stderr.write("Opening port %r failed: %s\n" % (serial.port, exception))
+        sys.exit(1)
+
+
+# function to turn all LEDs on on the display
+def all_leds_on():
+    serial.write("$$$ALL,ON\r")
+
+
+# function to turn all LEDs off on the display
+def all_leds_off():
+    serial.write("$$$ALL,OFF\r")
+
+
+# function to flash all LEDs on the display
+def flash_screen():
+    all_leds_on()
+    time.sleep(0.1)
+    all_leds_off()
+
+
+# function to feed and display data, where pattern data is 126 bits
+def map_to_display(pattern):
+    pattern = stringify(mirror_hor(rotate_cw(pattern)))
+    param = "$$$F" + pattern + "\r"
+    serial.write(param)
+
+
+# function to rotate the matrix clock-wise
+def rotate_cw(pattern):
+    return zip(*pattern[::-1])
+
+
+# function to mirror pattern horizontally
+def mirror_hor(input_pattern):
+    return numpy.fliplr(input_pattern)
+
+
+# function to concatenate and
+def stringify(matrix):
+    string = ""
+    for row in matrix:
+        string += str(row)\
+            .replace("[", "")\
+            .replace("]", "")\
+            .replace(",", "")\
+            .replace(" ", "")\
+            .replace("(", "")\
+            .replace(")", "")\
+            .replace("'", "")
+    return string
+
+
+test_pattern = ['11111111111111',
+                '10000000000001',
+                '10000000000001',
+                '10000000000001',
+                '10000000000001',
+                '10000000000001',
+                '10000000000001',
+                '10000000000001',
+                '11111111111111']
+
+init()
+flash_screen()
+map_to_display(sys.argv[0])
